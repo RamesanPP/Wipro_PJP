70a133ad96e81c60fb0e6ebc722b95228c9fc3fb
Ramesan PP <84701649+RamesanPP@users.noreply.github.com>
2022-04-21 00:59:38 +0530

    Update AJ51417T.py

--- Python/Project/AJ51417T.py
+++ Python/Project/AJ51417T.py
@@ -1,30 +1,33 @@
-from multiprocessing.pool import TERMINATE
+from os.path import exists
 
 
 class Employee:
-    count=0
     Empid=0
     Ename=""
     Sal=0
     Deptno=0 
-    def __init__(self, Empid, Ename, Sal, Deptno):
+    def __init__(self, Empid, Ename, Sal, Deptno, count):
         self.Empid = Empid
         self.Ename = Ename
         self.Sal = Sal
         self.Deptno = Deptno
-        Employee.count += 1
-    
-    
-    
+
+
+
 def Add_Emp():
     # Get empid from user and check if valid
-    empid=int(input("Enter the Empid:"))
+    empid=int(input("Enter the Empid: "))
     if (len(str(empid))<3):
         print("Should be 3 or more digits")
         return
-    if empid==Employee.Empid:
-        print("Should have Unique ID")
-        return
+    if exists("Project/emp.txt"):
+        with open("Project/emp.txt", 'r') as f:
+            l = f.readlines()
+            l = [x.strip() for x in l]
+            for line in l:
+                if line.startswith("Empid") and line.endswith(str(empid)):  
+                    print("Should have Unique ID")
+                    return
 
 
     # Get name from user and check if valid
@@ -40,7 +43,7 @@ def Add_Emp():
 
 
     # Get Deptno from user and check if valid
-    dep=int(input("Enter the DeptNo:"))
+    dep=int(input("Enter the DeptNo: "))
     if dep!=10 and dep!=20 and dep!=30:
         print("Should be either 10, 20 or 30")
         return
@@ -55,40 +58,55 @@ def Add_Emp():
 
 
 def Display_Emp():
-    print("Empid = ",Employee.Empid,"\nEname = ",Employee.Ename,"\nSal = ",Employee.Sal,"\nDeptno = ",Employee.Deptno)
+    if exists("Project/emp.txt"):
+        with open("Project/emp.txt") as f:
+            contents = f.read()
+            print (contents)
+    else:
+        print("No Employees")
 
 
 def Separate_Data():
-    with open("emp.txt") as file_in:
-        lines = []
-        for line in file_in:
-            lines.append(line)
-            if line.startswith("Deptno") and line.endswith("10"):
-                file = open("emp_10.txt","a")
-                file.write(lines)
-                file.close
-                lines.clear()
-            elif line.startswith("Deptno") and line.endswith("20"):
-                file = open("emp_20.txt","a")
-                file.write(lines)
-                file.close
-                lines.clear()
-            elif line.startswith("Deptno") and line.endswith("30"):
-                file = open("emp_30.txt","a")
-                file.write(lines)
-                file.close
-                lines.clear()
+    if exists("Project/emp.txt"):
+        with open("Project/emp.txt") as f:
+            l = f.readlines()
+            l = [x.strip() for x in l]
+            lines = []
+            for line in l:
+                lines.append(line)
+                if line.startswith("Deptno") and line.endswith("10"):
+                    file = open("Project/emp_10.txt","a")
+                    for s in lines:
+                        file.write(s + "\n")
+                    file.close
+                    lines.clear()
+                elif line.startswith("Deptno") and line.endswith("20"):
+                    file = open("Project/emp_20.txt","a")
+                    for s in lines:
+                        file.write(s + "\n")
+                    file.close
+                    lines.clear()
+                elif line.startswith("Deptno") and line.endswith("30"):
+                    file = open("Project/emp_30.txt","a")
+                    for s in lines:
+                        file.write(s + "\n")
+                    file.close
+                    lines.clear()
+
+    else:
+        print("No data")
 
   
 # Display Menu and get choice
-ch = int(input("1. Add_emp \n2. Display_emp \n3. Seperate_data \n4. Exit \n"))
-if ch == 1:
-    Add_Emp()
-elif ch == 2:
-    Display_Emp()
-elif ch == 3:
-    Separate_Data()
-else:
-    TERMINATE
-
+t = True
+while t:
+    ch = int(input("1. Add_emp \n2. Display_emp \n3. Separate_data \n4. Exit \n"))
+    if ch == 1:
+        Add_Emp()
+    elif ch == 2:
+        Display_Emp()
+    elif ch == 3:
+        Separate_Data()
+    else:
+        t = False
 
